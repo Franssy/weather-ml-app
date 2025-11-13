cafe8bd79ff1a8fd8734a07174f89ccffa0e87cf
Francis Nwoke <fnwoke300@caledonian.ac.uk>
2025-11-13 08:16:20 +0100

    Wrote tests and fixed some bugs.

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, re
 from app import app
 
 
@@ -10,10 +10,17 @@ class TestAppSmoke(unittest.TestCase):
     # Complete the function below to test a success in running the application
     def test_prediction_route_success(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200, "Expected route '/' to return 200 OK")
 
-    # Complete the function below to test a form is rendered
+
+# Complete the function below to test a form is rendered
     def test_get_form(self):
         response = self.client.get('/')
+        html = response.data.decode('utf-8').lower()
+        pattern = "<form [a-z=\" /]*>"
+        match = re.search(pattern, html)
+        self.assertTrue(match is not  None, "Expected an HTML form to be rendered on the main page")
+        self.assertIn('type="submit"', html, "Form should have a submit button")
 
 
 if __name__ == '__main__':
